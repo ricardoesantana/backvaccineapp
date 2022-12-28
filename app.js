abc6f6cd33ec7f4bf0daee2adf193ebfe9e4ee80
Ricardo Santana <ricardoesantana@gmail.com>
2022-12-28 03:25:06 -0300

    Atualização do CRUD do api

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 // Requiring module
 const express = require('express');
+const mysql = require('mysql');
+const connect = require('./conexao.js');
 
 // Creating express object 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Handling GET request
 app.get('/', (req, res) => {
     //res.send('A simple Node App is ' + 'running on this server')
-    res.send('A api de Ricardo Santana está rodando nesse servidor!')
-res.end() })
+    res.send('A api de Ricardo Santana está rodando nesse servidor!');
+    res.end();
+})
+
+app.get('/clientes', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery('select * from cliente', res);
+})
+
+app.get('/clientes/:id', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery('select * from cliente where id ='+ req.params.id, res);
+})
+
+app.put('/clientes/:id', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery("update cliente set nome='"+req.body.nome+"' where id="+req.params.id, res);
+})
+
+app.post('/clientes/', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery("insert into cliente values('"+req.body.id+"','"+req.body.nome+"')", res);
+})
+
+app.delete('/clientes/:id', (req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery("delete from cliente where id="+ req.params.id, res);
+})
 
 // Port Number
 const PORT = process.env.PORT ||5000;
 
 // Server Setup
-app.listen(PORT, console.log('Server started on port ${PORT}'))
+app.listen(PORT, console.log('Server started on port ${PORT}'));
---
+++ conexao.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+
+function execSQLQuery(sqlQry, res) {
+    const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'admin',
+    password: '123!@#',
+    database: 'node'
+});
+
+connection.query(sqlQry, function(error, results, fields){
+    if(error)
+        res.json(error);
+    else
+        res.json(results);
+        connection.end();
+        console.log('executou!');
+});
+
+}
+
+exports.execSQLQuery = execSQLQuery;
